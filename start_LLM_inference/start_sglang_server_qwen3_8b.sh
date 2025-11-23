94da543323c9e33efd90ffa48b404393a11ffbed
genglinliu <genglinliu@gmail.com>
2025-11-23 14:50:13 -0500

    update

--- start_LLM_inference/start_sglang_server_qwen3_8b.sh
+++ start_LLM_inference/start_sglang_server_qwen3_8b.sh
@@ -4,7 +4,7 @@ docker run --gpus '"device=4"' \
     --shm-size 32g \
     -p 30002:30002 \
     -v /fsx-neo/dedicated-fsx-data-repo-neo-us-east-1/genglin/.cache/huggingface:/root/.cache/huggingface \
-    --env "HF_TOKEN=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" \
+    --env "HF_TOKEN" \
     --ipc=host \
     lmsysorg/sglang:latest \
     bash -c "python3 -m sglang.launch_server --model-path Qwen/Qwen3-8B --host 0.0.0.0 --port 30002 --tp-size 1 --mem-fraction-static 0.7"
--- start_LLM_inference/start_sglang_server_qwen_vl_7b.sh
+++ start_LLM_inference/start_sglang_server_qwen_vl_7b.sh
@@ -1,10 +1,10 @@
-mkdir -p /fsx-neo/dedicated-fsx-data-repo-neo-us-east-1/genglin/.cache/huggingface
+mkdir -p /.cache/huggingface
 
 docker run --gpus '"device=5"' \
     --shm-size 32g \
     -p 30003:30003 \
-    -v /fsx-neo/dedicated-fsx-data-repo-neo-us-east-1/genglin/.cache/huggingface:/root/.cache/huggingface \
-    --env "HF_TOKEN=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" \
+    -v /.cache/huggingface \
+    --env "HF_TOKEN" \
     --ipc=host \
     lmsysorg/sglang:latest \
     bash -c "python3 -m sglang.launch_server --model-path Qwen/Qwen2.5-VL-7B-Instruct --host 0.0.0.0 --port 30003 --tp-size 1 --mem-fraction-static 0.7"
